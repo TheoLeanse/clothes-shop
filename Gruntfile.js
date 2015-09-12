2ca93e75cd90acbaefe13bcf60877cb9cc806c17
TheoLeanse <theoleanse@gmail.com>
2015-09-12 15:14:33 +0100

    re-config karma from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,8 @@ module.exports = function (grunt) {
         watch: { options: { spawn: false },
                  js: { files: sourceFiles,
                        tasks: ['newer:jshint', 'newer:jscs', 'karma'] } },
-        jscs: {},
-        jshint: {},
+        jscs: { options: { config: '.jscsrc' }, files: { src: sourceFiles } },
+        jshint: { options: { jshintrc: true }, files: { src: sourceFiles } },
         bower: { install: {} },
         concurrent: {
             options: {
@@ -20,7 +20,23 @@ module.exports = function (grunt) {
             },
             watch: ['watch:js']
         },
-        karma: { unit: { singleRun: true, options: { files: ['test/**/*Spec.js'] } } },
+        karma: {
+            unit: {
+                singleRun: true,
+                options: {
+                    files: [
+                        'test/**/*Spec.js'
+                    ],
+                    plugins: [
+                        'karma-chrome-launcher',
+                        'karma-jasmine',
+                        'karma-phantomjs-launcher'
+                    ],
+                    browsers: ['PhantomJS'],
+                    frameworks: ['jasmine']
+                }
+            }
+        },
         browserSync: {}
     });
     grunt.registerTask('default', ['bower', 'concurrent:watch', 'browserSync']);
