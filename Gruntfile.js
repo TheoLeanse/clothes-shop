a0d66c2aded84c0e6cf457e0f708f4d204a56f0b
TheoLeanse <theoleanse@gmail.com>
2015-09-12 13:38:38 +0100

    configure grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,9 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         githooks: {},
-        watch: {},
+        watch: { options: { spawn: false },
+                 js: { files: sourceFiles,
+                       tasks: ['newer:jshint', 'newer:jscs', 'karma'] } },
         jscs: {},
         jshint: {},
         bower: { install: {} },
