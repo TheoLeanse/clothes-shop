f88750819c046b179d9d31a4fcb324d518aa3f94
TheoLeanse <theoleanse@gmail.com>
2015-09-14 15:39:09 +0100

    add product list to Store Controller

--- javascripts/storeController.js
+++ javascripts/storeController.js
@@ -1,4 +1,5 @@
 clothesStore.controller('StoreController', function () {
     var self = this;
+    self.products = store.products;
     self.shoppingCart = [];
 });
--- test/spec/storeControllerSpec.js
+++ test/spec/storeControllerSpec.js
@@ -1,10 +1,14 @@
 describe('StoreController', function () {
-    beforeEach(module('ClothesStore'));;
     var ctrl;
+    beforeEach(module('ClothesStore'));;
     beforeEach(inject(function($controller) {
         ctrl = $controller('StoreController');
     }));
+    it('should have a list of products', function () {
+        expect(ctrl.products).toEqual(store.products);
+    });
     it('should have a shopping cart', function () {
         expect(ctrl.shoppingCart).toBeDefined();
     });
+    
 });
