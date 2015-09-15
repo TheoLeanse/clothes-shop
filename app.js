06f1267ffc0a40e2916c86f6b9a67d387f45192f
TheoLeanse <theoleanse@gmail.com>
2015-09-15 13:51:57 +0100

    handle heroku dynamic port assignment

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 var express = require('express'),
     app     = express();
-
-app.use(express.static(__dirname + '/public'))
-
-app.listen(8080, function () {
-    console.log('Clothes Store app listening at http://localhost:8080');
-});
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.PORT || 8080);
