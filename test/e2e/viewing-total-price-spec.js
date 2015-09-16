152cce743850522e7b0c463f1750e7c6f624c5fc
TheoLeanse <theoleanse@gmail.com>
2015-09-16 11:17:14 +0100

    add feature test for price display in cart

--- test/e2e/viewing-total-price-spec.js
+++ test/e2e/viewing-total-price-spec.js
@@ -1,6 +1,17 @@
 describe('Viewing an order\'s total price', function () {
-    var priceTagEl = element(by.binding('product.price'));
-    it('should have a price per item', function () {
-        expect(priceTagEl.getText()).toEqual('£99.00');
+    var storeEl = element(by.repeater('product in storeCtrl.products'));
+    var storePriceTagEl = storeEl.element(by.binding('product.price'));
+    var cartEl = element(by.repeater('cartItem in storeCtrl.shoppingCart'));
+    var cartPriceTagEl = cartEl.element(by.binding('cartItem.price'));
+//    var addItemBtn = element(by.buttonText('Add to cart'));
+    beforeEach(function () {
+        browser.get('http://localhost:8080');
+    });
+    it('should have a price per item in the store section', function () {
+        expect(storePriceTagEl.getText()).toEqual('£99.00');
+    });
+    it('should show item prices in the cart', function () {
+        element(by.buttonText('Add to cart')).click();
+        expect(cartPriceTagEl.getText()).toEqual('£99.00');
     });
 });
