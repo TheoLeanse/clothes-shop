7404a25503a8ffcd516dae6b0b35484e42b46daa
TheoLeanse <theoleanse@gmail.com>
2015-09-16 12:16:39 +0100

    add features test for cart total

--- test/e2e/viewing-total-price-spec.js
+++ test/e2e/viewing-total-price-spec.js
@@ -3,15 +3,22 @@ describe('Viewing an order\'s total price', function () {
     var storePriceTagEl = storeEl.element(by.binding('product.price'));
     var cartEl = element(by.repeater('cartItem in storeCtrl.shoppingCart'));
     var cartPriceTagEl = cartEl.element(by.binding('cartItem.price'));
-//    var addItemBtn = element(by.buttonText('Add to cart'));
+    var addItemBtn = element(by.buttonText('Add to cart'));
+    var totalPriceEl = element(by.binding('storeCtrl.cartTotal()'));
+    
     beforeEach(function () {
         browser.get('http://localhost:8080');
     });
+
     it('should have a price per item in the store section', function () {
         expect(storePriceTagEl.getText()).toEqual('£99.00');
     });
     it('should show item prices in the cart', function () {
         element(by.buttonText('Add to cart')).click();
         expect(cartPriceTagEl.getText()).toEqual('£99.00');
+    });
+    it('should show the total price of cart items with one item', function () {
+        element(by.buttonText('Add to cart')).click();
+        expect(totalPriceEl.getText()).toEqual('Total: £99.00');
     });
 });
