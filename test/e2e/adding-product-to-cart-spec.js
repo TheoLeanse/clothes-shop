ce3e395e1af334cb117c3021aaf9b6252c4e368b
TheoLeanse <theoleanse@gmail.com>
2015-09-17 09:09:01 +0100

    refactor protractor locators

--- test/e2e/adding-product-to-cart-spec.js
+++ test/e2e/adding-product-to-cart-spec.js
@@ -1,16 +1,20 @@
 describe('Adding a product to the shopping cart', function () {
-    var productList  = element.all(by.repeater('product in storeCtrl.products'));
-    var productName   = element(by.binding('product.name'));
-    var addProductBtn = element(by.buttonText('Add to cart'));
+    var items  = element.all(by.repeater('product in storeCtrl.products'));
+    var courtShoesElmnts = items.filter(function (elem) {
+        return elem.getText().then(function(text) {
+            return (text.indexOf('Court Shoes') > -1);
+        });
+    });
+    var buyCourtShoesBtn = courtShoesElmnts.get(0).element(by.buttonText('Add to cart'));
     var cartItemName  = element(by.binding('item.name'));
     beforeEach(function () {
         browser.get('http://localhost:8080');
     });
     it('should display product name(s)', function () {
-        expect(productName.getText()).toEqual('Almond Toe Court Shoes, Patent Black');
+        expect(courtShoesElmnts.get(0).element(by.binding('product.name')).getText()).toEqual('Almond Toe Court Shoes, Patent Black');
     });
     it('should allow a user to add a product to their cart', function () {
-        addProductBtn.click();
+        buyCourtShoesBtn.click();
 
         expect(cartItemName.getText()).toEqual('Almond Toe Court Shoes, Patent Black');
     });
